698f6d97d4e81303b547e28ad93ffb4f48a797ca
nikoss1161 <93620507+nikoss1161@users.noreply.github.com>
2025-11-03 01:05:08 +0200

    Add English language file for Quick Product Edit module

---
+++ upload/admin/language/en-gb/extension/module/quick_product_edit.php
@@ -0,0 +1,68 @@
+<?php
+// Heading
+$_['heading_title'] = 'Quick Product Edit';
+
+// Text
+$_['text_extension'] = 'Extensions';
+$_['text_success'] = 'Success: You have modified Quick Product Edit module!';
+$_['text_edit'] = 'Edit Quick Product Edit Module';
+$_['text_enabled'] = 'Enabled';
+$_['text_disabled'] = 'Disabled';
+$_['text_quick_edit'] = 'Quick Edit';
+$_['text_field_visibility'] = 'Field Visibility';
+$_['text_fullscreen'] = 'Fullscreen';
+$_['text_exit_fullscreen'] = 'Exit Fullscreen';
+$_['text_save_success'] = 'Product updated successfully!';
+$_['text_save_error'] = 'Error updating product!';
+$_['text_loading'] = 'Loading...';
+
+// Entry
+$_['entry_status'] = 'Status';
+
+// Button
+$_['button_save'] = 'Save';
+$_['button_cancel'] = 'Cancel';
+$_['button_close'] = 'Close';
+$_['button_edit'] = 'Edit';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify Quick Product Edit module!';
+
+// Field Labels
+$_['field_model'] = 'Model';
+$_['field_sku'] = 'SKU';
+$_['field_upc'] = 'UPC';
+$_['field_ean'] = 'EAN';
+$_['field_jan'] = 'JAN';
+$_['field_isbn'] = 'ISBN';
+$_['field_mpn'] = 'MPN';
+$_['field_location'] = 'Location';
+$_['field_price'] = 'Price';
+$_['field_quantity'] = 'Quantity';
+$_['field_minimum'] = 'Minimum Quantity';
+$_['field_status'] = 'Status';
+$_['field_shipping'] = 'Requires Shipping';
+$_['field_subtract'] = 'Subtract Stock';
+$_['field_weight'] = 'Weight';
+$_['field_length'] = 'Length';
+$_['field_width'] = 'Width';
+$_['field_height'] = 'Height';
+$_['field_seo_keyword'] = 'SEO Keyword';
+$_['field_sort_order'] = 'Sort Order';
+$_['field_name'] = 'Product Name';
+$_['field_description'] = 'Description';
+$_['field_meta_title'] = 'Meta Title';
+$_['field_meta_description'] = 'Meta Description';
+$_['field_meta_keyword'] = 'Meta Keywords';
+$_['field_tag'] = 'Product Tags';
+// admin/language/en-gb/extension/module/quick_product_edit.php
+$_['text_quick_create'] = 'Quick Create';
+$_['text_tab_general'] = 'General';
+$_['text_tab_data'] = 'Data';
+$_['text_tab_links'] = 'Links';
+$_['text_tab_attribute'] = 'Attributes';
+$_['text_tab_option'] = 'Options';
+$_['text_tab_image'] = 'Images';
+
+$_['text_confirm_delete'] = 'Are you sure?';
+$_['text_loading'] = 'Loading...';
